d682dc785fd74661dda2d9423770aca44ebdc6a0
Vasie <59412971+Vasie1337@users.noreply.github.com>
2025-09-24 10:22:25 +0200

    initial plan/commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module api-app
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,91 @@
+/*
+{
+    "object": "list",
+    "has_more": false,
+    "data": [
+      {
+        "object": "set",
+        "id": "5d293ad8-a749-4725-bd5c-c4e1db828bd0",
+        "code": "ecl",
+        "mtgo_code": "ecl",
+        "arena_code": "ecl",
+        "name": "Lorwyn Eclipsed",
+        "uri": "https://api.scryfall.com/sets/5d293ad8-a749-4725-bd5c-c4e1db828bd0",
+        "scryfall_uri": "https://scryfall.com/sets/ecl",
+        "search_uri": "https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e%3Aecl&unique=prints",
+        "released_at": "2026-01-23",
+        "set_type": "expansion",
+        "card_count": 0,
+        "digital": false,
+        "nonfoil_only": true,
+        "foil_only": true,
+        "icon_svg_uri": "https://svgs.scryfall.io/sets/default.svg?1758513600"
+      },
+      {
+        "object": "set",
+        "id": "a75e6ceb-d62c-4063-8e96-f269e3a0b025",
+        "code": "tle",
+        "mtgo_code": "tle",
+        "arena_code": "tle",
+        "tcgplayer_id": 24422,
+        "name": "Avatar: The Last Airbender Eternal",
+        "uri": "https://api.scryfall.com/sets/a75e6ceb-d62c-4063-8e96-f269e3a0b025",
+        "scryfall_uri": "https://scryfall.com/sets/tle",
+        "search_uri": "https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e%3Atle&unique=prints",
+        "released_at": "2025-11-21",
+        "set_type": "expansion",
+        "card_count": 101,
+        "parent_set_code": "tla",
+        "digital": false,
+        "nonfoil_only": false,
+        "foil_only": false,
+        "icon_svg_uri": "https://svgs.scryfall.io/sets/tle.svg?1758513600"
+      },
+
+*/
+
+/*
+Kolomnaam Scryfall json property
+
+Code code
+
+Name name
+
+API_url scryfall_uri
+
+Released released_at
+
+Icon_url Icon_svg_url
+*/
+
+// https://api.scryfall.com/sets
+
+// load all in json from get req
+// then parse to objects in mem
+/*
+Vereisten
+
+- Gebruik de juiste API die beschikbaar is om de sets op te halen.
+
+- Schrijf de sets weg naar een CSV-bestand.
+
+- Het uiteindelijke CSV-bestand moet de volgende kolommen bevatten:
+
+Kolomnaam Scryfall json property
+
+Code code
+
+Name name
+
+API_url scryfall_uri
+
+Released released_at
+
+Icon_url Icon_svg_url
+
+
+- Het bestand moet geordend zijn op releasedatum van de set.
+
+- Instructie hoe de applicatie gestart kan worden (en evt. geïnstalleerd als dit noodzakelijk is).
+*/
+
